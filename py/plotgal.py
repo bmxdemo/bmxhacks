d78d4e86916851baa81dffbab5b020372d547013
Anze Slosar <anze@bnl.gov>
2019-04-24 17:02:32 -0400

    playground

--- py/plotgal.py
+++ py/plotgal.py
@@ -8,11 +8,11 @@ import astropy.units as u
 from scipy.interpolate import interp1d
 from astropy.coordinates import EarthLocation, AltAz,SkyCoord
 from astropy.time import Time
-
+f21=1420.4
 
 telescope_loc=EarthLocation(lat=40.87792*u.deg, lon=-72.85852*u.deg, height=0*u.m)
 prepareData=True
-day='22'
+day=31
 
 
 def getB(da):
@@ -25,10 +25,12 @@ def getB(da):
     return b
 
 def main():
-    f=sorted(glob.glob("/gpfs01/astro/workarea/bmxdata/1708%s/1708%s_*.data"%(day,day)))
+    f=sorted(glob.glob("/gpfs01/astro/workarea/bmxdata/1708%02d/1708%02d_1*.data"%(day-1,day-1))+
+        glob.glob("/gpfs01/astro/workarea/bmxdata/1708%02d/1708%02d_2*.data"%(day-1,day-1))+
+        glob.glob("/gpfs01/astro/workarea/bmxdata/1708%02d/1708%02d_0[0,1,2,3,4,5]*.data"%(day,day)))
     imgX=None
-    bmin=-30
-    bmax=80
+    bmin=-22
+    bmax=30
     nB=(bmax-bmin)+1
     if prepareData:
         for fname in f:
@@ -38,12 +40,12 @@ def main():
             galb=getB(bf)
 
             if imgX is None:
-                imin,imax,fmin,fmax=bf.f2iminmax(1400.,1440.)
+                imin,imax,fmin,fmax=bf.f2iminmax(1415.,1424.)
                 nF=imax-imin
                 imgX=np.zeros((nF,nB))
                 imgY=np.zeros((nF,nB))
                 sc=np.zeros(nB)
-            for ib,b in enumerate(range(bmin,bmax)):
+            for ib,b in enumerate(range(bmin,bmax+1)):
                 #wh=np.where((not bf.radarOn) & (galb>b) & (galb<b+1))
                 wh=np.where((galb>b) & (galb<b+1))
                 if len(wh[0])>0:
@@ -53,15 +55,15 @@ def main():
                     sc[ib]+=len(wh[0])
         imgX/=np.outer(np.ones(nF),sc+1e-3)
         imgY/=np.outer(np.ones(nF),sc+1e-3)
-        np.save(day,(imgX,imgY))
+        np.save('g'+str(day),(imgX,imgY))
 
     import matplotlib.pyplot as plt
     import matplotlib.colors as colors
-    imgX,imgY=np.load("%s.npy"%day)
+    imgX,imgY=np.load("g%i.npy"%day)
     for i,img in enumerate([imgX,imgY]):
         ## fft filter
         print "FFT"
-        if True:
+        if False:
             t=np.fft.rfft2(img)
             t[:,30:]=0 # fast varying in b
             t[:,:2]=0 # non-varying in b
@@ -73,10 +75,10 @@ def main():
             img=np.fft.irfft2(t)
         
         plt.subplot(2,1,i+1)
-        plt.imshow(img.T,interpolation="nearest" , aspect = "auto", extent=[1400, 1440, bmin,bmax],origin='lower',vmin=-1e14,vmax=2e14)
+        plt.imshow(img.T,interpolation="nearest" , aspect = "auto", extent=[1415-f21, 1424-f21, bmin,bmax],origin='lower')
 
 
-        plt.xlabel("MHz")
+        plt.xlabel("$\Delta$ MHz")
         plt.ylabel("galactic b")
         plt.colorbar()
     plt.show()
